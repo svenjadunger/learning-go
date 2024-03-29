13d2bcf7cf4d930826da2b5e33a32dffbf5d9d22
Svenja Dunger <svenjadunger@web.de>
2024-03-29 17:28:11 +0100

    feat: complete loop task

--- project-ls/main.go
+++ project-ls/main.go
@@ -4,30 +4,27 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 )
 
 func main() {
-	// output := strings.Join(listFiles("testdata"))
+	files := listFiles("testdata") 
 
-	// fmt.Println(output)
-
-	output :=listFiles("testdata")
-	fmt.Println(strings.Join(output," "))
+	for _, file := range files { 
+		fmt.Println(file) 
+	}
 }
 
 func listFiles(dirname string) []string {
 	var dirs []string
 
 	files, err := os.ReadDir(dirname)
-	 
-	if err!= nil {
-		log.Fatal(err)
+	if err != nil {
+		log.Fatal(err) 
 	}
 
 	for _, f := range files {
-		dirs = append(dirs, f.Name())
+		dirs = append(dirs, f.Name()) 
 	}
 
-	return dirs
+	return dirs 
 }
